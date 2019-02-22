9a449e966bcfb33256d1ef677757ed145b03a9fc
Angela Montanye <Angela.montanye@gmail.com>
2019-02-21 20:46:22 -0700

    created migrations

---
+++ database/migrations/1550806281665_add_role_id_to_users_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AddRoleIdToUsersSchema extends Schema {
+  up () {
+    this.table('users', (table) => {
+      table.integer("role_id").unsigned().after('password')
+      table.foreign("role_id").references('users.id')
+    })
+  }
+
+  down () {
+    this.table('users', (table) => {
+      table.dropForeign('role_id')
+      table.dropColumn('role_id')
+    })
+  }
+}
+
+module.exports = AddRoleIdToUsersSchema
