4b78b8b377cf574315bb8e6bc4cc751b279a6b10
MichaelBruschwein <m.bruschwein16@gmail.com>
2019-03-05 18:56:29 -0700

    created delete function

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,16 +2,31 @@
 const User = use('App/Models/User')
 
 class UserController {
+    
     async createUser({request, auth, response}) {
-    const {username, email, password} = request.post()
-    const user = await User.create({username, email, password})
-    const token = await auth.generate(user)
-    console.log(user)
-    response.json({
-        message: '${user.username} added to the database', 
-        data:token
-    })
-}
+        const {username, email, password} = request.post()
+        const user = await User.create({username, email, password})
+        const token = await auth.generate(user)
+        console.log(user)
+        response.json({
+            message: '${user.username} added to the database', 
+            data:token
+        })
+    }
+
+    async deleteUser({ request, response, auth }) {
+        try {
+            let deleteUserById = await auth.getUser()
+           
+            await deleteUserById.delete()
+            response.json({
+                Message: "Time for Mickey D's"
+            })
+        } catch (e) {
+            response.send('Invalid token' + e)
+        }
+    }
+
 }
 
 module.exports = UserController
