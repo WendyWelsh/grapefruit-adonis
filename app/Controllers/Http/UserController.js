32a9f4eb9aca38e74fb856cbffdc17a8c92a3472
Angela Montanye <Angela.montanye@gmail.com>
2019-03-09 16:02:37 -0700

    fixed login

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,36 +1,58 @@
-'use strict'
-const User = use('App/Models/User')
+"use strict";
+const User = use("App/Models/User");
 
 class UserController {
-
-    async getUser({ request, response }) {
-        let users = await User.all()
-        response.send({
-            users: users
-        })
-    }
     
-    async createUser({request, auth, response}) {
-        const {username, email, password, role} = request.post();
-        const user = await User.create({username, email, password, role_id:parseInt(role)});
-        const token = await auth.generate(user);
-        console.log(user)
-        response.json({
-            message: '${user.username} added to the database', 
-            data:token
-        })
-    }
+  async getUser({ request, response }) {
+    let users = await User.all();
+    response.send({
+      users: users
+    });
+  }
 
-    async deleteUser({ request, response, params: { id } }) {
-        var deleteUserById = await User.find(id)
-        await deleteUserById.delete()
-        let users = await User.all()
-        response.json({
-            Message: "Time for Mickey D's!",
-            users: users
-        })
+  async login({ request, auth, response }) {
+    try {
+      const token = await auth
+        .withRefreshToken()
+        .attempt(request.input("email"), request.input("password"));
+      // return response.redirect('/home')
+      return response.json({
+        status: "success!",
+        data: token
+      });
+    } catch (error) {
+      response.status(400).json({
+        status: "error",
+        message: "Invalid Credentials"
+      });
     }
+  }
 
+  async createUser({ request, auth, response }) {
+    const { username, email, password, role } = request.post();
+    const user = await User.create({
+      username,
+      email,
+      password,
+      role_id: parseInt(role)
+    });
+    const token = await auth.generate(user);
+    console.log(user);
+    response.json({
+      message: "${user.username} added to the database",
+      data: token
+    });
+  }
+
+  async deleteUser({ request, response, params: { id } }) {
+    var deleteUserById = await User.find(id);
+    await deleteUserById.delete();
+    let users = await User.all();
+    response.json({
+      Message: "Time for Mickey D's!",
+      users: users
+    });
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
