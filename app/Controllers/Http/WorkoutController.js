1ca9620d62cf7f8e5c9a132883498ca78a879704
Matt Schmitt <mw.schmitt2@gmail.com>
2019-03-23 11:48:20 -0600

    injecting workout

--- app/Controllers/Http/WorkoutController.js
+++ app/Controllers/Http/WorkoutController.js
@@ -5,16 +5,16 @@ const Workout = use("App/Models/Workout")
 class WorkoutController {
 
     async storeWorkout({ request, auth, response }) {
-        // let user = await auth.getUser()
-        // const { client_id, coach_id, date } = request.post();
-        // const workout = await Workout.create({
-        //   client_id,
-        //   coach_id,
-        //   date
-        // });
-        // response.json({
-        //   message: "${user.username} is going to get ripped!"
-        // });
+        let user = await auth.getUser()
+        const { clientId, date } = request.post();
+        const workout = await Workout.create({
+          client_id: clientId,
+          coach_id: user.id,
+          date
+        });
+        response.json({
+          message: "${user.username} is going to get ripped!"
+        });
         return request.all()
     }
 
